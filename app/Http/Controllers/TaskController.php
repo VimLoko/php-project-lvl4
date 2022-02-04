424f6532f130a639f21ab3bd20ec072115e38d28
Proggit <proggit@mail.ru>
2022-02-04 20:13:07 +0500

    Created filter form

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -32,7 +32,7 @@ class TaskController extends Controller
                 AllowedFilter::exact('created_by_id'),
                 AllowedFilter::exact('assigned_to_id'),
             ])
-            ->orderBy('created_at', 'DESC')
+            ->orderBy('created_at', 'ASC')
             ->paginate(10);
         $filter = $request->filter ?? null;
         $statuses = TaskStatus::all()->pluck('name','id');
